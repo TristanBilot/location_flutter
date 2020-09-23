238f7a24f68f80d8d4cef306cf9e9f54bec5d071
Tristan BILOT <bilot.tristan@hotmail.fr>
2020-09-24 00:36:08 +0200

    refactor(): renaming files using Dart convention

--- lib/widgets/bottom_sheet.dart
+++ lib/widgets/bottom_sheet.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
-import 'bottomSheetContent.dart';
+import 'bottom_sheet_content.dart';
 
 class FloatingModal extends StatelessWidget {
   final Widget child;
