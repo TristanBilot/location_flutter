0c53bd75b8bec2e8d772209bada09c1cabf5e229
Tristan BILOT <tristan_bilot@carrefour.com>
2020-09-24 15:10:26 +0200

    feat(cach): o

--- lib/stores/cache_manager.dart
+++ lib/stores/cache_manager.dart
@@ -2,21 +2,25 @@ import 'dart:collection';
 import '../models/user.dart';
 
 class CacheManager {
-  HashMap<String, User> _cache;
+  static HashMap<String, User> _cache;
 
-  CacheManager() {
+  static void init() {
     _cache = HashMap();
   }
 
-  void putUser(User user) {
+  static void putUser(User user) {
     _cache[user.email] = user;
   }
 
-  User getUser(String id) {
+  static User getUser(String id) {
     return _cache[id];
   }
 
-  getUserImage(String id) {
+  static bool userExists(String id) {
+    return _cache.containsKey(id);
+  }
+
+  static getUserImage(String id) {
     final user = getUser(id);
     // DefaultCacheManager().getSingleFile(user.)
   }
