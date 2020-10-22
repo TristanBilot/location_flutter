92151b91da3dafaff7ad31f0819ff8ec53d8ad04
Tristan BILOT <tristan_bilot@carrefour.com>
2020-10-22 15:34:24 +0200

    feat(messaging): pull to refresh on chats page

--- lib/use_cases/messaging/chats_page.dart
+++ lib/use_cases/messaging/chats_page.dart
@@ -1,9 +1,11 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:location_project/stores/user_store.dart';
 import 'package:location_project/use_cases/messaging/chat_tile.dart';
 import 'package:location_project/use_cases/messaging/firestore_chat_entry.dart';
 import 'package:location_project/use_cases/messaging/messaging_repository.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
 import '../../stores/extensions.dart';
 
 class ChatsPage extends StatefulWidget {
@@ -15,10 +17,13 @@ class ChatsPage extends StatefulWidget {
 
 class _ChatsPageState extends State<ChatsPage> {
   Stream<QuerySnapshot> _chatsStream;
+  RefreshController _refreshController;
 
   @override
   void initState() {
     _fetchChatsStream();
+    _refreshController = RefreshController(initialRefresh: false);
+
     super.initState();
   }
 
@@ -38,6 +43,13 @@ class _ChatsPageState extends State<ChatsPage> {
     );
   }
 
+  void _onRefresh() async {
+    if (mounted) setState(() => {});
+    _refreshController.refreshCompleted();
+  }
+
+  void _onLoading() async => _refreshController.loadComplete();
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -45,14 +57,21 @@ class _ChatsPageState extends State<ChatsPage> {
         stream: _chatsStream,
         builder: (context, snapshot) {
           return snapshot.hasData
-              ? ListView.builder(
-                  itemCount: snapshot.data.documents.length,
-                  shrinkWrap: true,
-                  itemBuilder: (context, index) {
-                    return ChatTile(
-                        chat: _objectToEntry(
-                            snapshot.data.documents[index].data()));
-                  })
+              ? SmartRefresher(
+                  enablePullDown: true,
+                  controller: _refreshController,
+                  onRefresh: _onRefresh,
+                  onLoading: _onLoading,
+                  header: WaterDropMaterialHeader(), //WaterDropHeader
+                  child: ListView.builder(
+                      itemCount: snapshot.data.documents.length,
+                      shrinkWrap: true,
+                      itemBuilder: (context, index) {
+                        return ChatTile(
+                            chat: _objectToEntry(
+                                snapshot.data.documents[index].data()));
+                      }),
+                )
               : Container();
         },
       ),
