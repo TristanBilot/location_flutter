d978c3ccb7dfe90eb36b7e8d661f5668880dc22f
Tristan BILOT <tristan_bilot@carrefour.com>
2020-10-25 00:21:11 +0200

    feat(search): search works

--- lib/use_cases/messaging/chats_page.dart
+++ lib/use_cases/messaging/chats_page.dart
@@ -18,15 +18,19 @@ class ChatsPage extends StatefulWidget {
 class _ChatsPageState extends State<ChatsPage> {
   Stream<QuerySnapshot> _chatsStream;
   RefreshController _refreshController;
+  TextEditingController _messageEditingController;
 
   @override
   void initState() {
-    _fetchChatsStream();
+    _messageEditingController = TextEditingController();
     _refreshController = RefreshController(initialRefresh: false);
+    _fetchChatsStream();
 
     super.initState();
   }
 
+  /// Fetch the stream of chats and setState() to consume the
+  /// stream builder.
   Future<void> _fetchChatsStream() async {
     final userID = UserStore().user.id;
     final snapshots = await MessagingReposiory().getChats(userID);
@@ -36,11 +40,27 @@ class _ChatsPageState extends State<ChatsPage> {
     });
   }
 
+  /// Sort the snapshots in ascending last message order.
   List<dynamic> _sortSnapshotsByMostRecent(List<dynamic> snapshots) {
     final mostRecent = ChatField.LastActivityTime.value;
     return snapshots
       ..sort((a, b) => (b.data()[mostRecent] as int)
           .compareTo((a.data()[mostRecent] as int)));
+  }
+
+  /// Return the same list of snapshots with only the chats with
+  /// participants which the name match the string `pattern`.
+  /// This function is used for the search feature.
+  /// Return the same list of snapshots unchanged if `pattern` is empty.
+  List<dynamic> _filterStreamByName(List<dynamic> snapshots, String pattern) {
+    if (pattern.length == 0) return snapshots;
+    return snapshots.where((snapshot) {
+      final chat = FirestoreChatEntry.fromFirestoreObject(snapshot.data());
+      final otherParticipantName = (chat.userNames
+            ..removeWhere((userName) => userName == UserStore().user.firstName))
+          .first;
+      return otherParticipantName.toLowerCase().contains(pattern.toLowerCase());
+    }).toList();
   }
 
   void _onRefresh() async {
@@ -53,51 +73,56 @@ class _ChatsPageState extends State<ChatsPage> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Row(
+      child: Column(
         children: [
-          ConstrainedBox(
-            constraints: BoxConstraints(
-              maxHeight: 55.0,
-            ),
-            child: SingleChildScrollView(
-              scrollDirection: Axis.vertical,
-              reverse: true,
-              child: TextField(
-                keyboardType: TextInputType.multiline,
-                maxLines: null, //grow automatically
-                decoration: InputDecoration.collapsed(
-                  hintText: 'placeholder',
-                ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: CupertinoTextField(
+              padding: EdgeInsets.fromLTRB(12, 5, 12, 5),
+              controller: _messageEditingController,
+              onChanged: (text) => setState(() => {}),
+              style:
+                  TextStyle(color: Theme.of(context).textTheme.headline6.color),
+              // clearButtonMode: OverlayVisibilityMode.editing,
+              keyboardType: TextInputType.multiline,
+              maxLines: 1,
+              decoration: BoxDecoration(
+                color: Theme.of(context).scaffoldBackgroundColor,
+                border: Border.all(color: Colors.grey[500], width: 0.7),
+                borderRadius: BorderRadius.circular(10),
               ),
+              placeholder: 'Search someone',
             ),
           ),
-          StreamBuilder(
-            stream: _chatsStream,
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                // List a;
-                // a.where((element) => false);
-                // snapshot.data.where((a) => a.)
-                final chats =
-                    _sortSnapshotsByMostRecent(snapshot.data.documents);
-                return SmartRefresher(
-                  enablePullDown: true,
-                  controller: _refreshController,
-                  onRefresh: _onRefresh,
-                  onLoading: _onLoading,
-                  header: WaterDropMaterialHeader(), //WaterDropHeader
-                  child: ListView.builder(
-                      itemCount: chats.length,
-                      shrinkWrap: true,
-                      itemBuilder: (context, index) {
-                        return ChatTile(
-                            chat: FirestoreChatEntry.fromFirestoreObject(
-                                chats[index].data()));
-                      }),
-                );
-              }
-              return Container();
-            },
+          // Text('hello'),
+          Flexible(
+            child: StreamBuilder(
+              stream: _chatsStream,
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  final sortedChats =
+                      _sortSnapshotsByMostRecent(snapshot.data.documents);
+                  final chats = _filterStreamByName(
+                      sortedChats, _messageEditingController.text);
+                  return SmartRefresher(
+                    enablePullDown: true,
+                    controller: _refreshController,
+                    onRefresh: _onRefresh,
+                    onLoading: _onLoading,
+                    header: WaterDropMaterialHeader(), //WaterDropHeader
+                    child: ListView.builder(
+                        itemCount: chats.length,
+                        shrinkWrap: true,
+                        itemBuilder: (context, index) {
+                          return ChatTile(
+                              chat: FirestoreChatEntry.fromFirestoreObject(
+                                  chats[index].data()));
+                        }),
+                  );
+                }
+                return Container();
+              },
+            ),
           ),
         ],
       ),
