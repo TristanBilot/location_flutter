b4a3da7c59e7667a0301ef600f35ff7a3fbc5139
Tristan Bilot <tristan_bilot@carrefour.com>
2020-12-29 18:44:05 +0100

    fix(premium): persist users on likes page even with other states

--- lib/use_cases/premium/premium_likes_page.dart
+++ lib/use_cases/premium/premium_likes_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:location_project/models/user.dart';
 import 'package:location_project/use_cases/premium/widgets/premium_user_card.dart';
 import 'package:location_project/use_cases/tab_pages/counters/cubit/counters_cubit.dart';
 
@@ -10,21 +11,24 @@ class PremiumLikesPage extends StatefulWidget {
 
 class _PremiumLikesPageState extends State<PremiumLikesPage> {
   static final double spacing = 15.0;
+  List<User> _users;
+
+  Widget _gridViewWidget() => GridView.count(
+        crossAxisCount: 2,
+        crossAxisSpacing: spacing,
+        mainAxisSpacing: spacing,
+        padding: EdgeInsets.all(spacing),
+        children: _users.map((user) => PremiumUserCard(user)).toList(),
+      );
 
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<CountersCubit, CountersState>(builder: (context, state) {
       if (state is NewLikesState) {
-        final likes = state.likes;
-        return GridView.count(
-          crossAxisCount: 2,
-          crossAxisSpacing: spacing,
-          mainAxisSpacing: spacing,
-          padding: EdgeInsets.all(spacing),
-          children: likes.map((user) => PremiumUserCard(user)).toList(),
-        );
+        _users = state.likes;
+        return _gridViewWidget();
       }
-      return Container();
+      return _users != null ? _gridViewWidget() : Container();
     });
   }
 }
